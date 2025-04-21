e322ea2bde2346cacb1743d17499873ec8d85caa
acid0844 <re.birth.gr83@gmail.com>
2025-04-21 17:36:33 +0900

    feat: ポケモン選択で詳細表示

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -2,11 +2,19 @@
   <div>
     <h1>ポケモン図鑑（日本語）</h1>
     <ul>
-      <li v-for="poke in pokemons" :key="poke.id">
+      <li v-for="poke in pokemons" :key="poke.id" @click="showDetails(poke)">
         <img :src="poke.image" alt="pokemon image" width="100" height="100"/>
         {{ poke.japaneseName }}
       </li>
     </ul>
+
+    <div v-if="selectedPokemon">
+      <h2>{{ selectedPokemon.japaneseName }}の詳細</h2>
+      <img :src="selectedPokemon.image" alt="pokemon image" width="150" height="150"/>
+      <p>タイプ: {{ selectedPokemon.types.join(', ') }}</p>
+      <p>高さ: {{ selectedPokemon.height / 10 }} m</p>
+      <p>重さ: {{ selectedPokemon.weight / 10 }} kg</p>
+    </div>
   </div>
 </template>
 
@@ -14,7 +22,8 @@
 export default {
   data() {
     return {
-      pokemons: []
+      pokemons: [],
+      selectedPokemon: null
     }
   },
   async mounted() {
@@ -29,15 +38,26 @@ export default {
 
         const japaneseNameObj = speciesData.names.find(n => n.language.name === 'ja')
 
+        const detailsRes = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        const detailsData = await detailsRes.json()
+
         return {
           id,
           japaneseName: japaneseNameObj ? japaneseNameObj.name : pokemon.name,
-          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+          types: detailsData.types.map(type => type.type.name),
+          height: detailsData.height,
+          weight: detailsData.weight
         }
       })
     )
 
     this.pokemons = fetchedPokemons
+  },
+  methods: {
+    showDetails(pokemon) {
+      this.selectedPokemon = pokemon
+    }
   }
 }
 </script>
