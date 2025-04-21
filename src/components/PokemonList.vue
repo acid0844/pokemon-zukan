eddd3db6ce1613499937b610f1009656f18232ae
acid0844 <re.birth.gr83@gmail.com>
2025-04-21 17:25:03 +0900

    feat: PokéAPIを使ってポケモンの名前を羅列

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>データ一覧</h1>
+    <ul>
+      <li v-for="item in items" :key="item.name">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  mounted() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+      .then(response => response.json())
+      .then(data => {
+        this.items = data.results
+      })
+  }
+}
+</script>
